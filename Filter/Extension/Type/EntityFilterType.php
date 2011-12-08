7e3db1e5043e41e4b4b303ca5745c3c875f12d9a
Cédric G <c.girard@lexik.fr>
2011-12-08 10:36:32 +0100

    fix filter entity extension

--- Filter/Extension/Type/EntityFilterType.php
+++ Filter/Extension/Type/EntityFilterType.php
@@ -5,6 +5,7 @@ namespace Lexik\Bundle\FormFilterBundle\Filter\Extension\Type;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 use Doctrine\ORM\QueryBuilder;
+use Doctrine\Common\Collections\Collection;
 
 /**
  * Filter type for related entities.
@@ -35,14 +36,33 @@ class EntityFilterType extends EntityType implements FilterTypeInterface
     public function applyFilter(QueryBuilder $queryBuilder, $field, $values)
     {
         if (is_object($values['value'])) {
-            if (!is_callable(array($values['value'], 'getId'))) {
-                throw new \Exception(sprintf('Can\'t call method "getId()" on an instance of "%s"', get_class($values['value'])));
+            if ($values['value'] instanceof Collection) {
+                $ids = array();
+
+                foreach ($values['value'] as $value) {
+                    if (!is_callable(array($value, 'getId'))) {
+                        throw new \Exception(sprintf('Can\'t call method "getId()" on an instance of "%s"', get_class($value)));
+                    }
+                    $ids[] = $value->getId();
+                }
+
+                if (count($ids) > 0) {
+                    $joinAlias = substr($field, 0, 3);
+
+                    $queryBuilder->leftJoin(sprintf('%s.%s', $queryBuilder->getRootAlias(), $field), $joinAlias)
+                        ->andWhere($queryBuilder->expr()->in($joinAlias, $ids));
+                }
+
+            } else {
+                if (!is_callable(array($values['value'], 'getId'))) {
+                    throw new \Exception(sprintf('Can\'t call method "getId()" on an instance of "%s"', get_class($values['value'])));
+                }
+
+                $paramName = sprintf(':%s_param', $field);
+
+                $queryBuilder->andWhere(sprintf('%s.%s = %s', $queryBuilder->getRootAlias(), $field, $paramName))
+                    ->setParameter($paramName, $values['value']->getId(), \PDO::PARAM_INT);
             }
-
-            $paramName = sprintf(':%s_param', $field);
-
-            $queryBuilder->andWhere(sprintf('%s.%s = %s', $queryBuilder->getRootAlias(), $field, $paramName))
-                ->setParameter($paramName, $values['value']->getId(), \PDO::PARAM_INT);
         }
     }
 }
