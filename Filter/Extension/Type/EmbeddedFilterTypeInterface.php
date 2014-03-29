374055ee6165cac1cf73fa1bf60478d9fc0745e6
Eduardo García Sanz <eduardo@comakai.com>
2014-03-29 21:01:38 +0100

    Adding support for embedded object (http://www.doctrine-project.org/jira/browse/DDC-2773)

---
+++ Filter/Extension/Type/EmbeddedFilterTypeInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Lexik\Bundle\FormFilterBundle\Filter\Extension\Type;
+
+/**
+ * Some filter type can implement this interface to apply the filter to a property from an embedded object.
+ */
+interface EmbeddedFilterTypeInterface
+{
+}
