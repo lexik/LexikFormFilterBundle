fcd2dfd41ea23b309b9c0611b999512e712689c8
damlys <damian.lysiak@gmail.com>
2018-02-23 15:37:24 +0100

    allow fields to not be required

--- Filter/DataExtractor/Method/ValueKeysExtractionMethod.php
+++ Filter/DataExtractor/Method/ValueKeysExtractionMethod.php
@@ -44,8 +44,6 @@ class ValueKeysExtractionMethod implements DataExtractionMethodInterface
                         $values['value'][$key][$k] = $v;
                     }
                 }
-            } else {
-                throw new \InvalidArgumentException(sprintf('No value found for key "%s" in form data.', $key));
             }
         }
 
